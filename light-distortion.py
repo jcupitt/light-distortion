240f53a22608c48236f3961160c95e75359e033f
John Cupitt <jcupitt@gmail.com>
2025-03-10 10:24:57 +0000

    improve code a bit

--- light-distortion.py
+++ light-distortion.py
@@ -1,51 +1,43 @@
-from pyvips.vimage import Image
-import pyvips
+#!/usr/bin/env python3
+
 import sys
-from typing import Tuple, List
 import re
+from typing import Tuple, List
+
+import pyvips
 
 Point = List[int]
 Couple = tuple[Point, Point]
 
 
-def shepards(image: Image, couples: List[Couple]):
-
-    if (len(couples) == 0):
-        return
-
-    index = Image.xyz(image.width, image.height)
+def shepards(image: pyvips.Image, couples: List[Couple]):
+    index = pyvips.Image.xyz(image.width, image.height)
     deltas = []
     weights = []
 
     for p1, p2 in couples:
-
         diff = index - p2
 
         distance = (diff[0]**2 + diff[1]**2)
-        distance = distance.ifthenelse(distance, 0.1)
 
-        weight = distance ** -1
-
-        delta = [(p2[0] - p1[0]), (p2[1] - p1[1])] * weight * -1
-
+        weight = (distance < 1.0).ifthenelse(1.0, 1.0 / distance)
         weights.append(weight)
+
+        delta = [(p1[0] - p2[0]), (p1[1] - p2[1])] * weight
         deltas.append(delta)
 
     # add, normalize
-    index += sum(deltas) * sum(weights) ** -1
+    index += pyvips.Image.sum(deltas) / pyvips.Image.sum(weights)
 
     return image.mapim(index, interpolate=pyvips.Interpolate.new('bicubic'))
 
 
 if __name__ == "__main__":
-    image = Image.new_from_file(sys.argv[1])
+    image = pyvips.Image.new_from_file(sys.argv[1])
 
-    couples = []
-    matches = re.findall("(\d+),(\d+) (\d+),(\d+)", sys.argv[3])
-    for match in matches:
-        couple = [[int(match[0]), int(match[1])],
-                  [int(match[2]), int(match[3])]]
-        couples.append(couple)
+    matches = re.findall(r'(\d+),(\d+) (\d+),(\d+)', sys.argv[3])
+    couples = [[[int(match[0]), int(match[1])], [int(match[2]), int(match[3])]]
+               for match in matches]
 
     image = shepards(image, couples)
     image.write_to_file(sys.argv[2])
